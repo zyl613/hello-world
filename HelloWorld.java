f98d80cbf1f34f7e17009b4901cb448994135989
zhangyl <1185876084@qq.com>
2016-06-03 13:24:49 +0800

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package com.jk.demo;
+
+/**
+ * Created by etu6 on 2016/6/3.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
